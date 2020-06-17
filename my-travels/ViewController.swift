13cbbca1a2cd7b81f30735c58724f22dcda05981
Ilana Quintino da Silva <ilanaquintinodasilva@gmail.com>
2020-06-17 13:18:49 -0300

    Initial Commit

---
+++ my-travels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  my-travels
+//
+//  Created by Ilana Quintino da Silva on 17/06/20.
+//  Copyright © 2020 Ilana Quintino da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
